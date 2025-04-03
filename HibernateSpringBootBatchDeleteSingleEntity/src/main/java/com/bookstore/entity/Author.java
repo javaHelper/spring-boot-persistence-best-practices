cabc65adcc8619224eac8649cdc9a49679f23a10
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-03 19:45:55 +0530

    Updated to Spring Boot 3.2.10

--- HibernateSpringBootBatchDeleteSingleEntity/src/main/java/com/bookstore/entity/Author.java
+++ HibernateSpringBootBatchDeleteSingleEntity/src/main/java/com/bookstore/entity/Author.java
@@ -1,11 +1,13 @@
 package com.bookstore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Version;
+
 
 @Entity
 public class Author implements Serializable {
