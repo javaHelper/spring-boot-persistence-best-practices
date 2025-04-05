4820349c5a1e46e55ec9c7630e2b52068b389837
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-05 12:40:47 +0530

    Updated to Spring Boot 3.2.10

--- HibernateSpringBootHiLo/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootHiLo/src/main/java/com/bookstore/service/BookstoreService.java
@@ -2,8 +2,10 @@ package com.bookstore.service;
 
 import com.bookstore.entity.Author;
 import com.bookstore.repository.AuthorRepository;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,16 +18,12 @@ public class BookstoreService {
     }
 
     public void batch1000Authors() {
-
         List<Author> authors = new ArrayList<>();
-
         for (int i = 1; i <= 1000; i++) {
             Author author = new Author();
             author.setName("Author_" + i);
-
             authors.add(author);
         }
-
         authorRepository.saveAll(authors);
     }
 }
