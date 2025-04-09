e2a7c59e3326767d5153d6359b41030b0e91c18f
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-09 22:46:11 +0530

    Updated to Spring Boot 3.2.10

--- chapter-3/HibernateSpringBootSessionRepeatableReads/src/main/java/com/bookstore/dao/Dao.java
+++ chapter-3/HibernateSpringBootSessionRepeatableReads/src/main/java/com/bookstore/dao/Dao.java
@@ -2,8 +2,8 @@ package com.bookstore.dao;
 
 import java.io.Serializable;
 import java.util.Optional;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
