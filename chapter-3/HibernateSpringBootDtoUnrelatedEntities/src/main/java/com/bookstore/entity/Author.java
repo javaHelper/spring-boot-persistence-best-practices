61089255828dca2f5247b9810e9822d12f5fb8f6
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-11 01:21:43 +0530

    Updated to Spring Boot 3.2.10

--- chapter-3/HibernateSpringBootDtoUnrelatedEntities/src/main/java/com/bookstore/entity/Author.java
+++ chapter-3/HibernateSpringBootDtoUnrelatedEntities/src/main/java/com/bookstore/entity/Author.java
@@ -1,12 +1,14 @@
 package com.bookstore.entity;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity 
+import java.io.Serializable;
+
+
+@Entity
 public class Author implements Serializable {
 
     private static final long serialVersionUID = 1L;
