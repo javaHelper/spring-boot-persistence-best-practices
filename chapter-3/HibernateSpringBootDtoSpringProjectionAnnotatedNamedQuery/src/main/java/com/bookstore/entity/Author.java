88a4976cc3275435075206fe5400ecc585bf39a6
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-10 13:41:26 +0530

    Updated to Spring Boot 3.2.10

--- chapter-3/HibernateSpringBootDtoSpringProjectionAnnotatedNamedQuery/src/main/java/com/bookstore/entity/Author.java
+++ chapter-3/HibernateSpringBootDtoSpringProjectionAnnotatedNamedQuery/src/main/java/com/bookstore/entity/Author.java
@@ -1,11 +1,12 @@
 package com.bookstore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 
 @NamedQuery(
         name = "Author.fetchName",
