4510d75a2f5b97939370bec105a0a9891ff5d940
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-11 17:51:26 +0530

    Updated to Spring Boot 3.2.10

--- chapter-4/HibernateSpringBootBatchDeleteOrphanRemoval/src/main/java/com/bookstore/entity/Author.java
+++ chapter-4/HibernateSpringBootBatchDeleteOrphanRemoval/src/main/java/com/bookstore/entity/Author.java
@@ -1,16 +1,17 @@
 package com.bookstore.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Version;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Version;
 
 @Entity
 public class Author implements Serializable {
