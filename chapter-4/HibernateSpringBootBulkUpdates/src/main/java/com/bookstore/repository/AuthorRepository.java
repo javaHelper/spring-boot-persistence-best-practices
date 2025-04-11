8fed444b3e716085db7878edaefb70811966a3b2
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-11 18:21:06 +0530

    Updated to Spring Boot 3.2.10

--- chapter-4/HibernateSpringBootBulkUpdates/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter-4/HibernateSpringBootBulkUpdates/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -2,12 +2,9 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Author;
 import java.util.List;
-import javax.persistence.QueryHint;
-import static org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    @QueryHints(value = @QueryHint(name = HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    //@QueryHints(value = @QueryHint(name = HINT_PASS_DISTINCT_THROUGH, value = "false"))
     @Query("SELECT DISTINCT a FROM Author a JOIN FETCH a.books b WHERE a.age > ?1")
     List<Author> findGtGivenAge(int age);
 
