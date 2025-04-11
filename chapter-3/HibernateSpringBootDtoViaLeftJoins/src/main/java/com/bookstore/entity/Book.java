0790bab2b6b77d089a17e9146da4ca8ca25d9c77
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-11 12:30:21 +0530

    Updated to Spring Boot 3.2.10

--- chapter-3/HibernateSpringBootDtoViaLeftJoins/src/main/java/com/bookstore/entity/Book.java
+++ chapter-3/HibernateSpringBootDtoViaLeftJoins/src/main/java/com/bookstore/entity/Book.java
@@ -1,13 +1,14 @@
 package com.bookstore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Book implements Serializable {
@@ -23,7 +24,8 @@ public class Book implements Serializable {
     private int price;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
+    @JoinColumn(name = "author_id"
+    )
     private Author author;
 
     public Long getId() {
