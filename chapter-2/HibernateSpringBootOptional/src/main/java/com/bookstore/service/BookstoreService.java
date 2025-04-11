5fdbe563b91f357939792e5c7f2f62ff12b51d42
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-11 13:31:12 +0530

    Updated to Spring Boot 3.2.10

--- chapter-2/HibernateSpringBootOptional/src/main/java/com/bookstore/service/BookstoreService.java
+++ chapter-2/HibernateSpringBootOptional/src/main/java/com/bookstore/service/BookstoreService.java
@@ -14,9 +14,7 @@ public class BookstoreService {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
-    public BookstoreService(AuthorRepository authorRepository,
-            BookRepository bookRepository) {
-
+    public BookstoreService(AuthorRepository authorRepository, BookRepository bookRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
     }
