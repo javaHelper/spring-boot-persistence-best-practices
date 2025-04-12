1f6df7d097e8a9ca9178a8966efa63f796199368
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-12 13:45:26 +0530

    Updated to Spring Boot 3.2.10

--- chapter-7/HibernateSpringBootUUID2/src/main/java/com/bookstore/entity/Author.java
+++ chapter-7/HibernateSpringBootUUID2/src/main/java/com/bookstore/entity/Author.java
@@ -2,10 +2,11 @@ package com.bookstore.entity;
 
 import java.io.Serializable;
 import java.util.UUID;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
