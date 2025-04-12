7704b331588be6d61608a0afabecfe2c5aed67d4
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-12 13:49:25 +0530

    Updated to Spring Boot 3.2.10

--- chapter-7/HibernateSpringBootAutoGeneratorType/src/main/java/com/bookstore/entity/AuthorGood.java
+++ chapter-7/HibernateSpringBootAutoGeneratorType/src/main/java/com/bookstore/entity/AuthorGood.java
@@ -1,10 +1,11 @@
 package com.bookstore.entity;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -13,7 +14,7 @@ public class AuthorGood implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
     @GenericGenerator(name="native", strategy="native")
     private Long id;
 
