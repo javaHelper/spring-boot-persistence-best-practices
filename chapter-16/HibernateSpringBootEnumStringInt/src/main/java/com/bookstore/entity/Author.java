2dc3dbd57f72b4042deb6320960269094205975c
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-13 16:18:01 +0530

    Updated to Spring Boot 3.2.10

--- chapter-16/HibernateSpringBootEnumStringInt/src/main/java/com/bookstore/entity/Author.java
+++ chapter-16/HibernateSpringBootEnumStringInt/src/main/java/com/bookstore/entity/Author.java
@@ -1,14 +1,15 @@
 package com.bookstore.entity;
 
 import com.bookstore.enums.GenreType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Author implements Serializable {
