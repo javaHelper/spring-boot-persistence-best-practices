9c7e6b0211037dbfe9d516206fc0e222912d5758
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-08-06 20:52:11 +0530

    Spring Boot 3.5.3

--- chapter-8/HibernateSpringBootJpqlFunction/src/main/java/com/bookstore/entity/Book.java
+++ chapter-8/HibernateSpringBootJpqlFunction/src/main/java/com/bookstore/entity/Book.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 @Entity
 public class Book implements Serializable {
 
@@ -19,42 +21,4 @@ public class Book implements Serializable {
     private String title;
     private String isbn;
     private int price;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" + "id=" + id + ", title=" + title + ", isbn=" + isbn + ", price=" + price + '}';
-    }
-
 }
