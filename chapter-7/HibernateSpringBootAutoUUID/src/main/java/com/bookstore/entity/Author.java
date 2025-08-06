01248c14f71984d9132b3a616686edb77547aefb
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-08-06 23:05:14 +0530

    Spring Boot 3.5.3

--- chapter-7/HibernateSpringBootAutoUUID/src/main/java/com/bookstore/entity/Author.java
+++ chapter-7/HibernateSpringBootAutoUUID/src/main/java/com/bookstore/entity/Author.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.UUID;
 
+@Data
 @Entity
 public class Author implements Serializable {
 
@@ -22,41 +24,4 @@ public class Author implements Serializable {
     private int age;
     private String name;
     private String genre;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }   
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" + "id=" + id + ", age=" + age + ", name=" + name + ", genre=" + genre + '}';
-    }
 }
