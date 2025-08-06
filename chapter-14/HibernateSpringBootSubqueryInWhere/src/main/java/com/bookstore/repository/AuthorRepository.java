83a42081c387ba5091a0d600fbde2d9adc8a63bf
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-08-06 22:28:06 +0530

    Spring Boot 3.5.3

--- chapter-14/HibernateSpringBootSubqueryInWhere/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter-14/HibernateSpringBootSubqueryInWhere/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Author;
 import java.util.List;
+
+import com.bookstore.entity.Bestseller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     @Transactional(readOnly = true)
-    @Query("SELECT a FROM Author a WHERE a.id IN "
-            + "(SELECT b.authorId FROM Bestseller b)")
+    @Query("SELECT a FROM Author a WHERE a.id IN (SELECT b.authorId FROM Bestseller b)")
     public List<Author> fetchTheBest();
 
     // this is faster
