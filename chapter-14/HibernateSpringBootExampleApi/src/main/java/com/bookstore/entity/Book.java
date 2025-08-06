ca54b8c2b1c34e6fc30626456e98ca98e433b84a
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-08-06 22:23:09 +0530

    Spring Boot 3.5.3

--- chapter-14/HibernateSpringBootExampleApi/src/main/java/com/bookstore/entity/Book.java
+++ chapter-14/HibernateSpringBootExampleApi/src/main/java/com/bookstore/entity/Book.java
@@ -1,11 +1,14 @@
 package com.bookstore.entity;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+import java.io.Serializable;
+
+@Data
 @Entity
 public class Book implements Serializable {
 
@@ -20,52 +23,4 @@ public class Book implements Serializable {
     private String isbn;
     private String author;
     private int price;
-    
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }        
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }        
 }    
