b9de7b639629c2f92295cfde694ce02d3255c828
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-10-16 21:23:05 +0530

    Spring Boot 3.5.3

--- chapter-9/HibernateSpringBootLogSlowQueries545/src/main/java/com/bookstore/service/BookstoreService.java
+++ chapter-9/HibernateSpringBootLogSlowQueries545/src/main/java/com/bookstore/service/BookstoreService.java
@@ -2,16 +2,14 @@ package com.bookstore.service;
 
 import com.bookstore.entity.Author;
 import com.bookstore.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookstoreService {
 
     private final AuthorRepository authorRepository;
-
-    public BookstoreService(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
 
     public void persistAuthor() {
         Author author = new Author();
