39e63ebf37a2c9c3bc00bfa4b9a705e30795954b
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-10-16 21:17:27 +0530

    Spring Boot 3.5.3

--- chapter-9/HibernateSpringBootLogSlowQueries/src/main/java/com/bookstore/MainApplication.java
+++ chapter-9/HibernateSpringBootLogSlowQueries/src/main/java/com/bookstore/MainApplication.java
@@ -22,7 +22,6 @@ public class MainApplication {
     @Bean
     public ApplicationRunner init() {
         return args -> {
-            
             bookstoreService.persistAuthor();
             bookstoreService.displayAuthor();
         };
--- chapter-9/HibernateSpringBootLogSlowQueries/src/main/java/com/bookstore/entity/Author.java
+++ chapter-9/HibernateSpringBootLogSlowQueries/src/main/java/com/bookstore/entity/Author.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 @Entity
 public class Author implements Serializable {
 
@@ -20,41 +22,8 @@ public class Author implements Serializable {
     private String name;
     private String genre;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
-        return "Author{" + "id=" + id + ", age=" + age
-                + ", name=" + name + ", genre=" + genre + '}';
+        return "Author{" + "id=" + id + ", age=" + age + ", name=" + name + ", genre=" + genre + '}';
     }
 }
