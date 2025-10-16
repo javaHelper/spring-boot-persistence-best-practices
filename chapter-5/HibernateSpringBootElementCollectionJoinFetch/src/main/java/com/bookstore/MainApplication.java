4bb1f96ce03ec861d79f56098206d154d982451a
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-10-16 21:07:04 +0530

    Spring Boot 3.5.3

--- chapter-5/HibernateSpringBootElementCollectionJoinFetch/src/main/java/com/bookstore/MainApplication.java
+++ chapter-5/HibernateSpringBootElementCollectionJoinFetch/src/main/java/com/bookstore/MainApplication.java
@@ -1,20 +1,18 @@
 package com.bookstore;
 
-import com.bookstore.service.ShoppingCartService;
 import com.bookstore.entity.ShoppingCart;
+import com.bookstore.service.ShoppingCartService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class MainApplication {
 
     private final ShoppingCartService shoppingCartService;
-
-    public MainApplication(ShoppingCartService shoppingCartService) {
-        this.shoppingCartService = shoppingCartService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
--- chapter-5/HibernateSpringBootElementCollectionJoinFetch/src/main/java/com/bookstore/entity/Book.java
+++ chapter-5/HibernateSpringBootElementCollectionJoinFetch/src/main/java/com/bookstore/entity/Book.java
@@ -1,54 +1,23 @@
 package com.bookstore.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 @Embeddable
 public class Book implements Serializable {
 
     private static final long serialVersionUID = 1L;
-  
+
     private String title;
     private String isbn;
     private String genre;
-    private int price;  
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
+    private int price;
 
     @Override
     public String toString() {
-        return "Book{" + "title=" + title + ", isbn=" + isbn 
-                + ", genre=" + genre + ", price=" + price + '}';
-    }   
+        return "Book{" + "title=" + title + ", isbn=" + isbn + ", genre=" + genre + ", price=" + price + '}';
+    }
 }
