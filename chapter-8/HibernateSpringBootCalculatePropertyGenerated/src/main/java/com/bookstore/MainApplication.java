dca10f891fe8a3eae5259fc8fdac0058924bf032
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-11-02 12:05:04 +0530

    Spring Boot 3.5.3

--- chapter-8/HibernateSpringBootCalculatePropertyGenerated/src/main/java/com/bookstore/MainApplication.java
+++ chapter-8/HibernateSpringBootCalculatePropertyGenerated/src/main/java/com/bookstore/MainApplication.java
@@ -1,20 +1,17 @@
 package com.bookstore;
 
-import com.bookstore.entity.Book;
 import com.bookstore.service.BookstoreService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class MainApplication {
 
     private final BookstoreService bookstoreService;
-
-    public MainApplication(BookstoreService bookstoreService) {
-        this.bookstoreService = bookstoreService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
